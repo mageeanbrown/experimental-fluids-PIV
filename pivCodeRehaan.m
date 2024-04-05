8acb84f53155b99908810215283ca4454f312f8f
mageean <mageean@mageeans-mbp.devices.brown.edu>
2024-04-05 13:57:33 -0400

    Change the normalization method to what he said in analysis section - might want to double check it works properly

--- pivCodeRehaan.m
+++ pivCodeRehaan.m
@@ -8,13 +8,14 @@ clc
 image_a = im2double(imread('500fps_1000ss_rehaan_maggie_C001H001S0001000050.bmp'));
 image_b = im2double(imread('500fps_1000ss_rehaan_maggie_C001H001S0001000051.bmp'));
 
+image_dimensions = size(image_a); %in pixels
+image_size = image_dimensions(1); %assuming square image
+
 %Subtract average intensity
 image_a = image_a - mean(image_a,'all'); 
 image_b = image_b - mean(image_b,'all');
 
 %Select IA size (recommended 64x64 or 48x48)
-image_dimensions = size(image_a); %in pixels, assuming square image
-image_size = image_dimensions(1);
 IA_size = 64; %in pixels
 IA_overlap = IA_size*0.25; 
 
@@ -31,21 +32,32 @@ window)...uncomment to run
 %}
 % window = IA_size*IA_size;
 
- 
+%Initializing matrices
+x_vel = zeros(size(28,61));
+y_vel = zeros(size(28,61));
+correlation_value = zeros(size(28,61));
+velocity_magnitude = zeros(size(correlation_value));
+correlation_ratios = zeros(size(27,61));
+
+%Looping over the IAs find cross correlation between the image pairs
 for i = 1:(image_size-IA_size)/IA_overlap+1
     for j = 1:(image_size-IA_size)/IA_overlap+1
        
         window_a = image_a(1+(i-1)*IA_overlap: (i-1)*IA_overlap+IA_size, 1+(j-1)*IA_overlap: (j-1)*IA_overlap+IA_size).*window;
         window_b = image_b(1+(i-1)*IA_overlap: (i-1)*IA_overlap+IA_size, 1+(j-1)*IA_overlap: (j-1)*IA_overlap+IA_size).*window;
-        correlation = normxcorr2(window_a, window_b);
-        % 
-        % %Normalize the correlation
-        % ACFTbl1 = autocorr(window_a,10);
-        % ACFTbl2 = autocorr(window_b,10);
-        % correlation = correlation/(sqrt(ACFTbl1 * ACFTbl2));
-        % 
+        correlation = xcorr2(window_a, window_b);
+        
+        %Normalizing the correlation
+        %First find autocorr of each IA
+        autocorr_a = xcorr2(window_a,window_a);
+        autocorr_a = autocorr_a(IA_size,IA_size); %use center value
+        autocorr_b = xcorr2(window_b,window_b);
+        autocorr_b = autocorr_b(IA_size,IA_size);
+        %Normalize using sqrt of the autocorrelations
+        normalize = sqrt(autocorr_a*autocorr_b);
+        norm_correlation = correlation/normalize;
         [x, y] = ndgrid((-IA_size + 1):(IA_size - 1));
-        interpolation = griddedInterpolant(x, y, correlation,'spline');
+        interpolation = griddedInterpolant(x, y, norm_correlation,'spline');
         [peaks, value] = fminsearch(@(f) -interpolation(f(1), f(2)), [0, 0]);
 
         x_vel(i, j) = peaks(2);
@@ -55,10 +67,10 @@ for i = 1:(image_size-IA_size)/IA_overlap+1
 
         %Function below found from this forum
         % https://uk.mathworks.com/matlabcentral/answers/277512-how-to-find-peaks-in-3d-mesh
-        ix = find(imregionalmax(correlation));
-        list = sort(correlation(ix), 'descend');
+       % ix = find(imregionalmax(norm_correlation));
+       % list = sort(norm_correlation(ix), 'descend');
 
-        correlation_ratios(i,j) = list(1)/list(2);
+       % correlation_ratios(i,j) = list(1)/list(2);
 
     end
 end
@@ -147,10 +159,10 @@ for i = 1:(image_size-IA_size)/IA_overlap+1
        
         window_a = image_a(1+(i-1)*IA_overlap: (i-1)*IA_overlap+IA_size, 1+(j-1)*IA_overlap: (j-1)*IA_overlap+IA_size).*window;
         window_b = image_b(1+(i-1)*IA_overlap: (i-1)*IA_overlap+IA_size, 1+(j-1)*IA_overlap: (j-1)*IA_overlap+IA_size).*window;
-        correlation = normxcorr2(window_a, window_b);
+        norm_correlation = normxcorr2(window_a, window_b);
   
         [x, y] = ndgrid((-IA_size + 1):(IA_size - 1));
-        interpolation = griddedInterpolant(x, y, correlation,'spline');
+        interpolation = griddedInterpolant(x, y, norm_correlation,'spline');
         [peaks, value] = fminsearch(@(f) -interpolation(f(1), f(2)), [0, 0]);
 
         x_vel(i, j) = peaks(2);
@@ -160,8 +172,8 @@ for i = 1:(image_size-IA_size)/IA_overlap+1
 
         %Function below found from this forum
         % https://uk.mathworks.com/matlabcentral/answers/277512-how-to-find-peaks-in-3d-mesh
-        ix = find(imregionalmax(correlation));
-        list = sort(correlation(ix), 'descend');
+        ix = find(imregionalmax(norm_correlation));
+        list = sort(norm_correlation(ix), 'descend');
 
         correlation_ratios(i,j) = list(1)/list(2);
 
